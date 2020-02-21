1cd2115dc00c9561f5bb5c2650833d02e9624308
Arash <Arash20628@yahoo.com>
2020-02-21 23:48:46 +0330

    Add Chat View Controller

---
+++ Hey-Guys/Controller/ChatViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ChatViewController.swift
+//  Hey-Guys
+//
+//  Created by Arash on 12/1/1398 AP.
+//  Copyright © 1398 Arash. All rights reserved.
+//
+
+import UIKit
+
+class ChatViewController: UIViewController {
+    
+    //MARK: - Outlets
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var chatTextField: UITextField!
+    @IBOutlet weak var sendButton: UIButton!
+    @IBOutlet weak var heightConstraint: NSLayoutConstraint!
+    
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+  
+        
+    }
+    
+
+
+}
